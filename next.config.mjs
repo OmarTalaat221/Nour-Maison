25dfca2ab356a4dc64a15276d37f85c2e0c0b6e5
Mohamed <me782003f@gmail.com>
2025-12-06 21:22:55 +0200

    fix build error

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  devServer: {
-    host: '0.0.0.0',
-  },
-
   images: {
-    domains: ["res.cloudinary.com" ,  "lh3.googleusercontent.com"], // 👈 أضف الدومين هنا
+    domains: ["res.cloudinary.com", "lh3.googleusercontent.com"],
   },
 };
 
