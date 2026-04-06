15389da7e00d9f4f6e3e3779bcc02a2ed17542a1
OmarTalaat221 <omartalaat1276@gmail.com>
2026-04-06 13:36:19 +0200

    blogs

--- app/sitemap.xml/route.js
+++ app/sitemap.xml/route.js
@@ -4,35 +4,39 @@ import slugify from "../../lib/slugify";
 
 export async function GET() {
   const baseUrl = "https://www.nourmaison.co.uk";
-  const response = await fetch(
-    "https://camp-coding.tech/nour_maison/user/get_custom_blogs_data.php"
-  );
-  const blogs = await response.json();
 
-  const staticPages = [
-    "",
-    "/about-us",
-    "/contact-us",
-    "/menu-gallery",
-    "/menu",
-    "/blog",
-    "/gallery",
-    "/kids-menu",
-    "/roast-menu",
-    "/ramadan-iftar-menu-milton-keynes",
-    // "/christmas-menu",
-    "/booking",
-    "/store",
-    "/terms-and-conditions",
-    "/privacy-policy-2",
-    "/refund-policy",
-    "/services",
-    "/afternoon-tea-menu",
-    "/all-blogs",
-    "/afternoon-tea-booking",
-  ];
+  try {
+    const response = await fetch(
+      "https://camp-coding.tech/nour_maison/user/get_custom_blogs_data.php"
+    );
+    const data = await response.json();
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    // Extract blogs array from the response
+    const blogs = data?.message?.blogs || [];
+
+    const staticPages = [
+      "",
+      "/about-us",
+      "/contact-us",
+      "/menu-gallery",
+      "/menu",
+      "/blog",
+      "/gallery",
+      "/kids-menu",
+      "/roast-menu",
+      "/ramadan-iftar-menu-milton-keynes",
+      "/booking",
+      "/store",
+      "/terms-and-conditions",
+      "/privacy-policy-2",
+      "/refund-policy",
+      "/services",
+      "/afternoon-tea-menu",
+      "/all-blogs",
+      "/afternoon-tea-booking",
+    ];
+
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
   <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
     ${staticPages
       .map(
@@ -40,32 +44,47 @@ export async function GET() {
         <url>
           <loc>${baseUrl}${page}</loc>
           <lastmod>${new Date().toISOString()}</lastmod>
-        </url>
-      `
+          <changefreq>weekly</changefreq>
+          <priority>${page === "" ? "1.0" : "0.8"}</priority>
+        </url>`
       )
       .join("")}
 
-    ${blogs?.message
+    ${blogs
       .map(
         (blog) => `
         <url>
-          <loc>${baseUrl}/blog/${blog.id}/${slugify(
-            blog?.keywords || blog.title
-          )}</loc>
-<lastmod>${
-          blog.updated_at
-            ? new Date(blog.updated_at).toISOString()
-            : new Date().toISOString()
-        }</lastmod>
-        </url>
-      `
+          <loc>${baseUrl}/blog/${blog.id}/${slugify(blog?.keywords || blog.title)}</loc>
+          <lastmod>${new Date().toISOString()}</lastmod>
+          <changefreq>monthly</changefreq>
+          <priority>0.6</priority>
+        </url>`
       )
       .join("")}
   </urlset>`;
 
-  return new Response(sitemap, {
-    headers: {
-      "Content-Type": "application/xml",
-    },
-  });
+    return new Response(sitemap, {
+      headers: {
+        "Content-Type": "application/xml",
+        "Cache-Control": "public, max-age=3600, s-maxage=3600",
+      },
+    });
+  } catch (error) {
+    console.error("Sitemap generation error:", error);
+
+    // Return a minimal sitemap with just static pages if API fails
+    const fallbackSitemap = `<?xml version="1.0" encoding="UTF-8"?>
+  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+    <url>
+      <loc>${baseUrl}</loc>
+      <lastmod>${new Date().toISOString()}</lastmod>
+    </url>
+  </urlset>`;
+
+    return new Response(fallbackSitemap, {
+      headers: {
+        "Content-Type": "application/xml",
+      },
+    });
+  }
 }
