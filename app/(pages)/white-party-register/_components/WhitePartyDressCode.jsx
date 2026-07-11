bedc737361dcfdeec7391ae9818e5f579e4e6a5a
OmarTalaat221 <omartalaat1276@gmail.com>
2026-07-11 17:16:49 +0300

    white party

--- app/(pages)/white-party-register/_components/WhitePartyDressCode.jsx
+++ app/(pages)/white-party-register/_components/WhitePartyDressCode.jsx
@@ -3,7 +3,7 @@
 import { motion } from "framer-motion";
 import Image from "next/image";
 import { Link as ScrollLink } from "react-scroll";
-import BottomBg from "../../../../utils/BottomBg/BottomBg"; // ← عدّل الـ path حسب مكان الكومبوننت الفعلي
+import BottomBg from "../../../../utils/BottomBg/BottomBg";
 
 const DRESS_CODE_BG = "/images/dress-code-bg.webp";
 const DRESS_CODE_ALT =
